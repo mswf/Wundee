bcff113427deaec13879047a3721ba25bd166f69
Steff Kempink <mswf@live.nl>
2016-06-21 15:28:37 +0200

    * changed needRateEffect to amount/second instead of amount/tick

--- WundeeUnity/Assets/Scripts/Ingame/Stories/StoryElements/Effects/RateEffect.cs
+++ WundeeUnity/Assets/Scripts/Ingame/Stories/StoryElements/Effects/RateEffect.cs
@@ -25,7 +25,24 @@ namespace Wundee.Stories
 
 		public override void ExecuteEffect()
 		{
-			parentStoryNode.parentStory.parentSettlement.needs[_needIndex].amount += _rate;
+			parentStoryNode.parentStory.parentSettlement.needs[_needIndex].amount += _rate * Time.fixedDT;
+		}
+	}
+
+	public class NeedAmountEffect : BaseEffect, IRateEffect
+	{
+		private double _amount;
+		private int _needIndex;
+
+		public override void ParseParams(JsonData parameters)
+		{
+			_amount = ContentHelper.ParseDouble(parameters, D.AMOUNT, 0d);
+			_needIndex = ContentHelper.ParseNeedIndex(parameters, D.NEED);
+		}
+
+		public override void ExecuteEffect()
+		{
+			parentStoryNode.parentStory.parentSettlement.needs[_needIndex].amount += _amount;
 		}
 	}
 
